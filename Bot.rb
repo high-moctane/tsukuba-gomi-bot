76329bfe7e4174bbba69490bb7c4c3d069d56b97
high-moctane <sakaya_ppkds_yety-highoctane10492@yahoo.co.jp>
2015-11-11 21:41:02 +0900

    基本的な機能を実装した。

--- Bot.rb
+++ Bot.rb
@@ -8,24 +8,24 @@ require "pp"
 # Bot module
 module Bot
   class Bot
-    attr_reader :twitter
+    attr_reader :twitter, :stream
 
     def initialize(app_name)
       # ディレクトリ構造を考え付いたらここを書き換える
-      keys = YAML.load_file("./config/#{app_name}.yml")
+      keys = YAML.load_file("./#{app_name}_config.yml")
 
       @twitter = Twitter::REST::Client.new do |config|
-        config.consumer_key        = keys["consumer_key"]
-        config.consumer_secret     = keys["consumer_secret"]
-        config.access_token        = keys["access_token"]
-        config.access_token_secret = keys["access_token_secret"]
+        config.consumer_key        = keys[:consumer_key]
+        config.consumer_secret     = keys[:consumer_secret]
+        config.access_token        = keys[:access_token]
+        config.access_token_secret = keys[:access_token_secret]
       end
 
       TweetStream.configure do |config|
-        config.consumer_key       = keys["consumer_key"]
-        config.consumer_secret    = keys["consumer_secret"]
-        config.oauth_token        = keys["access_token"]
-        config.oauth_token_secret = keys["access_token_secret"]
+        config.consumer_key       = keys[:consumer_key]
+        config.consumer_secret    = keys[:consumer_secret]
+        config.oauth_token        = keys[:access_token]
+        config.oauth_token_secret = keys[:access_token_secret]
         config.auth_method        = :oauth
       end
 
@@ -36,6 +36,10 @@ module Bot
       @stream.userstream do |status|
         printf("%s\n\n", status.text)
       end
+    end
+
+    def post(string)
+      @twitter.update(string)
     end
   end
 end
