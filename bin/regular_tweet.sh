d47ea45c7df068a33651fe3b9b6ce9ef586566c0
high-moctane <high.moctane@gmail.com>
2015-11-17 23:27:01 +0900

    bundle がインストールされてないと言われる問題を修正

--- bin/regular_tweet.sh
+++ bin/regular_tweet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-# my_path=`readlink -f $0`
 my_path=`dirname $0`
 
-bundle exec ruby ${my_path}/../lib/regular_tweet.rb
+cd ${my_path}/../lib
+bundle exec ruby regular_tweet.rb
