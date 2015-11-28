895c61414f13b30ddf69f8f0eed4b98b458f715b
high-moctane <high.moctane@gmail.com>
2015-11-29 01:32:24 +0900

    なるべく綺麗に書いた 納得がいかないけど動くからいいとする

--- lib/extend_date.rb
+++ lib/extend_date.rb
@@ -1,34 +1,60 @@
 # Date を拡張する
-# TODO: とりあえず実装したので後でいい方法に書き換える
+
+# NOTE: モジュール使ってやろうとすると p とか pp とかがエラー吐いてダメ
 
 require "date"
+require_relative "bot/project"
 
-class Date
-  alias :__to_s__ :to_s
-  def to_s(lang = nil)
-    case lang
-    when :ja
-      "#{self.day}日(#{%w(日 月 火 水 木 金 土)[self.wday]})"
-    else
-      __to_s__
+include Bot::Project
+
+P = Bot::Project
+
+module Extend_Date
+  def __strings__
+    <<-'EOS'
+    alias :__to_s__ :to_s
+    def to_s(lang = nil)
+      case lang
+      when :ja
+        "#{self.day}日(#{P.lang[:ja][:day_name][self.wday]})"
+      when :en
+        str =
+          case day.to_s.split(//).pop
+          when "1" then :st
+          when "2" then :nd
+          when "3" then :rd
+          else :th
+          end
+        "#{P.lang[:en][:day_name][self.wday]}, #{self.day}#{str}"
+      else
+        __to_s__
+      end
     end
+    EOS
   end
 end
+
+include Extend_Date
+
+
+class Date
+  include Extend_Date
+  eval Extend_Date.__strings__
+end
+
 
 
 class DateTime
-  alias :__to_s__ :to_s
-  def to_s(lang = nil)
-    case lang
-    when :ja
-      "#{self.day}日(#{%w(日 月 火 水 木 金 土)[self.wday]})"
-    else
-      __to_s__
-    end
-  end
+  include Extend_Date
+  eval Extend_Date.__strings__
 end
+
+
 
 if $0 == __FILE__
   p today = Date.today
+  p today.to_s
   p today.to_s(:ja)
+  p today.to_s(:en)
 end
+
