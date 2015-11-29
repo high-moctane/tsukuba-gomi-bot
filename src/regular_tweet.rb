fb47215eda1617631b81d6b6f263a779de2c19f5
high-moctane <high.moctane@gmail.com>
2015-11-30 01:06:25 +0900

    多言語機能を廃止 想像以上にめんどくさかった

--- src/regular_tweet.rb
+++ src/regular_tweet.rb
@@ -16,16 +16,9 @@ warn "#{$0} 起動"
 
 now       = DateTime.now
 garb      = Bot::Garbage.new(now)
-lang_data = p.lang
 
 
 shift = now.hour.between?(0, 11) ? 0 : 1
-
-lang = [
-  Array.new(8, :ja),
-  Array.new(2, :en),
-].flatten.sample
-
 
 unless garb.any_collect?(shift: shift)
   p.log.info($0) { "ごみ収集なしのためつぶやかない" }
@@ -34,20 +27,11 @@ unless garb.any_collect?(shift: shift)
 end
 
 
-garb.localize(lang)
 
-
-message =
-  if shift == 0
-    lang_data[lang][:today].to_s
-  else
-    lang_data[lang][:tomorrow].to_s
-  end
-
-message << <<"EOS"
-: #{(now + shift).to_lang(lang)}
+message = <<"EOS"
+#{%w(今日 明日)[shift]} #{(now + shift).to_lang(:ja)}
 #{garb.day(shift: shift).map { |o| o * ": " } * "\n" }
-#{lang_data[lang][:footer]} #{now.strftime("%H:%M")}
+です(｀･ω･´) #{now.strftime("%H:%M")}
 EOS
 
 
