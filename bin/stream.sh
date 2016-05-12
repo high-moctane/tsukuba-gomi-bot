6984205ad01674264bc46b24c3851bbf9d978dbb
high-moctane <sakaya_ppkds_yety-highoctane10492@yahoo.co.jp>
2016-05-12 10:04:45 +0900

    書き直しに伴っていらないファイルを削除

--- bin/stream.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-my_path=`dirname $0`
-
-cd ${my_path}/../src
-bundle exec ruby stream.rb
--- lib/bot.rb
+++
@@ -1,9 +0,0 @@
-require_relative "bot/bot"
-require_relative "bot/garbage"
-require_relative "bot/project"
-require_relative "bot/message"
-
-if $0 == __FILE__
-  include Bot
-  pp Bot::Project.root_dir
-end
