5ec2d3379290ec356efd6f47852bc849243a5bfd
high-moctane <high.moctane@gmail.com>
2017-09-10 18:17:29 +0900

    とりあえずこんな感じの構成ということで golang ファイルをおいた

---
+++ gomibot/gomibot.go
@@ -0,0 +1,6 @@
+package gomibot
+
+// Run starts gomibot.
+func Run() int {
+	return 0
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os"
+
+	"github.com/high-moctane/tsukuba-gomi-bot/gomibot"
+)
+
+func main() {
+	os.Exit(gomibot.Run())
+}
