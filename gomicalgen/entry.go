ba1a96b8d12881909ede9fd82c22365ea219a4e2
high-moctane <high.moctane@gmail.com>
2019-07-07 17:07:25 +0900

    gomicalgen のエントリーに地区を含めることにした

--- gomicalgen/entry.go
+++ gomicalgen/entry.go
@@ -5,33 +5,34 @@ import (
 	"time"
 )
 
+var Districts = []string{"北地区", "西地区", "東地区", "南地区"}
+
 var Garbages = []string{
 	"収集なし",
 	"燃やせるごみ",
 	"燃やせないごみ",
-	"プラ製容器包装",
-	"粗大ごみ（予約制）",
+	"プラ製容器包装，粗大ごみ（予約制）",
 	"古紙・古布",
-	"びん",
-	"スプレー容器",
+	"びん，スプレー容器",
 	"ペットボトル",
 	"かん",
 }
 
 type Entry struct {
-	Date    string `json:"date"`
-	Garbage string `json:"garbage"`
+	Date     string `json:"date"`
+	District string `json:"district"`
+	Garbage  string `json:"garbage"`
 }
 
 type Calendar []Entry
 
-func (c *Calendar) Add(date time.Time, garbage string) {
+func (c *Calendar) Add(date time.Time, district, garbage string) {
 	if c == nil {
 		cal := make(Calendar, 0)
 		c = &cal
 	}
 	datestr := date.Format("2006-01-02")
-	*c = append(*c, Entry{datestr, garbage})
+	*c = append(*c, Entry{datestr, district, garbage})
 }
 
 func (c *Calendar) Dump() ([]byte, error) {
--- gomicalgen/main.go
+++ gomicalgen/main.go
@@ -10,31 +10,41 @@ import (
 )
 
 func main() {
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("セーブしました")
+}
+
+func Run() error {
 	dstdir, err := Dstdir()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	filename, date, err := GetFilenameDate()
+	date, err := GetDate()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	cal, err := MakeCalendar(filename, date)
+	datestr := GetDateyearStr(date)
+	filename := GetFilename(datestr)
+
+	cal, err := MakeCalendar(date)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	json, err := cal.Dump()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	if err := SaveJson(dstdir, filename, json); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	fmt.Println("セーブしました")
+	return nil
 }
 
 func Dstdir() (string, error) {
@@ -56,60 +66,61 @@ func Dstdir() (string, error) {
 	return dstdir, nil
 }
 
-func GetFilenameDate() (string, time.Time, error) {
+func GetDate() (time.Time, error) {
 	var yearmonth string
 	fmt.Println("年-月を入力してください")
 	fmt.Println("例: 2006-01")
 	fmt.Print("> ")
 	if _, err := fmt.Scan(&yearmonth); err != nil {
-		return "", time.Now(), err
+		return time.Now(), err
 	}
 	date, err := time.Parse("2006-01", yearmonth)
 	if err != nil {
 		fmt.Println("日付のフォーマットが不正です")
-		return "", time.Now(), fmt.Errorf("illegal format: %s", yearmonth)
+		return time.Now(), fmt.Errorf("illegal format: %s", yearmonth)
 	}
 
-	var district string
-	fmt.Println("地区を入力してください")
-	fmt.Println("例: north")
-	fmt.Print("> ")
-	if _, err := fmt.Scan(&district); err != nil {
-		return "", time.Now(), err
-	}
-	if !(district == "north" || district == "south" || district == "west" || district == "east") {
-		fmt.Println("地区は north, south, west, east のいずれかにしてください")
-		return "", time.Now(), fmt.Errorf("illegal district: %s", district)
-	}
-
-	return yearmonth + "_" + district + ".json", date, nil
+	return date, nil
 }
 
-func MakeCalendar(filename string, startDate time.Time) (Calendar, error) {
-	cal := Calendar{}
-	date := startDate
-	for date.Month() == startDate.Month() {
-		fmt.Println(`コマンド: "n" 次の日, 整数 ごみの種類`)
-		for i := 0; i < len(Garbages); i++ {
-			fmt.Printf("[%d] %s\n", i, Garbages[i])
-		}
-		fmt.Println("日付:", date.Format("2006-01-02"))
-		fmt.Print("> ")
+func GetDateyearStr(date time.Time) string {
+	return date.Format("2006-01")
+}
 
-		var cmd string
-		if _, err := fmt.Scan(&cmd); err != nil {
-			return nil, err
-		}
-		if cmd == "n" {
+func GetFilename(dateyearstr string) string {
+	return dateyearstr + ".json"
+}
+
+func MakeCalendar(startDate time.Time) (Calendar, error) {
+	cal := Calendar{}
+	for _, district := range Districts {
+		date := startDate
+		for date.Month() == startDate.Month() {
+			fmt.Println("地区:", district)
+			fmt.Println(`コマンド: 整数 ごみの種類`)
+			for i := 0; i < len(Garbages); i++ {
+				fmt.Printf("[%d] %s\n", i, Garbages[i])
+			}
+			fmt.Println("日付:", date.Format("2006-01-02"))
+			fmt.Print("> ")
+
+			var cmd string
+			if _, err := fmt.Scan(&cmd); err != nil {
+				return nil, err
+			}
+			if cmd == "n" {
+				continue
+			}
+			cmdint, err := strconv.ParseInt(cmd, 10, 64)
+			if err != nil || cmdint < 0 || len(Garbages) <= int(cmdint) {
+				fmt.Println("不正な入力です: ", cmd)
+				continue
+			}
+			fmt.Println(Garbages[cmdint])
+			fmt.Println("")
+			cal.Add(date, district, Garbages[cmdint])
 			date = date.Add(24*time.Hour + 10*time.Minute) // なんかうるう秒とかあったら嫌なので
-			continue
 		}
-		cmdint, err := strconv.ParseInt(cmd, 10, 64)
-		if err != nil || cmdint < 0 || len(Garbages) <= int(cmdint) {
-			fmt.Println("不正な入力です: ", cmd)
-			continue
-		}
-		cal.Add(date, Garbages[cmdint])
 	}
 	return cal, nil
 }
