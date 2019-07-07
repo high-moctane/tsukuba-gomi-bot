85f0f7cfb728f505a3e98022dd1fd7bdcc77a263
high-moctane <high.moctane@gmail.com>
2019-07-08 01:30:17 +0900

    重複投稿防止の時間表示を入れた

--- gomibot/message.go
+++ gomibot/message.go
@@ -16,11 +16,15 @@ func dateFormat(date time.Time) string {
 	return date.Format("1月2日（") + JapaneseWeekDay[date.Format("Mon")] + "）"
 }
 
+func timeFormat(t time.Time) string {
+	return t.Format("03:04")
+}
+
 func TodayMessage(c Calendar) string {
 	today := time.Now()
 	ans := "今日" + dateFormat(today) + "のごみは\n"
 	ans += c.Date(today) + "\n"
-	ans += "です(｀･ω･´)"
+	ans += "です(｀･ω･´) " + timeFormat(time.Now())
 	return ans
 }
 
@@ -28,6 +32,6 @@ func TomorrowMessage(c Calendar) string {
 	tomorrow := time.Now().Add(24 * time.Hour)
 	ans := "明日" + dateFormat(tomorrow) + "のごみは\n"
 	ans += c.Date(tomorrow) + "\n"
-	ans += "です(｀･ω･´)"
+	ans += "です(｀･ω･´) " + timeFormat(time.Now())
 	return ans
 }
